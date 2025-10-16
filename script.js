41813f7830e3be821e246e376105b7434377fc2d
WO| coltexe <brawl.colt.dina@gmail.com>
2025-10-16 14:52:50 +0000

    java

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+
+const prenom = "Johan";
+let age = 16;
+console.log(prenom,age)
+
+age=54548548484844
+console.log(age)
+
+   // prenom="fabrice"
+  //  console.log(prenom)  Uncaught TypeError: Assignment to constant variable. at script.js:12:11
+
+if (age < 13) {
+    console.log("Accès interdit aux moins de 13 ans.");
+} else if (age < 18) {
+    console.log("Vous pouvez accéder à la section ado");
+
+} else {
+    console.log("16ans");
+}
+
